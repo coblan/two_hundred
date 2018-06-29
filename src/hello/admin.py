e82520551aefa60aef6962860f7a718f39769bcc
he_yulin <he_yulin@163.com>
2018-06-29 13:10:22 +0800

    开发主页

--- src/hello/admin.py
+++ src/hello/admin.py
@@ -1,1 +1,16 @@
-print('hello is ok')
+from helpers.director.shortcut import FieldsPage, page_dc
+
+class home(FieldsPage):
+    template = 'hello/home.html'
+    def get_label(self): 
+        return '主页'
+    
+    def get_context(self): 
+        return {
+            
+        }
+
+
+page_dc.update({
+    'home': home,
+})
